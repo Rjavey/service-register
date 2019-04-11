3e0ccc360f36ecaa9ad71ab599205a91674b0134
Rjavey <1099779409@qq.com>
2019-04-11 14:31:07 +0800

    1

---
+++ src/main/java/com/cloud/serviceregister/ServiceRegisterApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.serviceregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRegisterApplication.class, args);
+    }
+
+}
